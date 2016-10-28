ef6fa30435e61117f1960446dfd511c099418526
Brendan <b101@umbc.edu>
2016-10-28 16:22:25 -0400

    Fixed the algorithm

--- proj1.py
+++ proj1.py
@@ -30,12 +30,11 @@ def OPT(i, j):
 
     for t in range(j-5, i-1, -1):
 
-        #print("Pair? (" + letters[t] + ", " + letters[j] + ")")
         if not validPair(letters[t], letters[j]):
             subresult, subresultList = OPT(i, j-1)
         else:
             subresult1, subresultList1 = OPT(i, t-1)
-            subresult2, subresultList2 = OPT(t+1, j)
+            subresult2, subresultList2 = OPT(t+1, j-1)
             subresult =  subresult1 + subresult2 + 1
             subresultList = subresultList1 + subresultList2 + [(t, j)]
 
@@ -43,44 +42,7 @@ def OPT(i, j):
             result = subresult
             resultList = subresultList
 
-    print("OPT(" + str(i) + ", " + str(j) + ") returning " + str(result) + ", resultList:", resultList)
     return result, resultList
-    
-
-"""
-def OPT(i, j):
-
-    
-    total = 0
-    listA = letters[i::-1]
-    listB = letters[j:]
-
-    if len(listB) == 0:
-        return total
-
-#    print(listA)
-#    print(listB)
-
-    #checks to make sure the sizes are compatible
-    if len(listA) < len(listB) or len(listA) == len(listB):
-        for x in range(len(listA)):
-            if validPair(listA[x], listB[x]):
-                total += 1
-
-    #checks to make sure the sizes are compatible
-    if len(listB) < len(listA):
-        for x in range(len(listB)):
-            if validPair(listA[x], listB[x]):
-                total += 1
-
-    print(total, "\n")
-    result = OPT(i+1, j+1)
-    
-    if total > result:
-        return total
-    else:
-        return result
-""" 
 
 def validPair(x, y):
     if x == "H" and y == "G":
